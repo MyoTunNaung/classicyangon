9c634b9ee14459b6247076623d3027990382cbf6
AungSitPaiOo-Frederick <aungsitpaioo1500love@gmail.com>
2025-12-31 11:47:28 +0630

    Initial Laravel 10 setup

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Classic Yangon | Professional Profile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container py-5">
+    <h1 class="fw-bold">Myo Tun Naung</h1>
+    <p class="lead">
+        Professional Developer & Computer Technology Educator
+    </p>
+
+    <hr>
+
+    <p>
+        Founder of Classic Computer Center | Laravel Developer | Education Technology Specialist
+    </p>
+</div>
+
+</body>
+</html>
