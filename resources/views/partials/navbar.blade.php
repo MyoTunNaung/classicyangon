37e422ff70906323a925a415774ca280fa7364b3
AungSitPaiOo-Frederick <aungsitpaioo1500love@gmail.com>
2026-01-01 01:58:27 +0630

    Navbar update

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,40 +1,25 @@
-<nav class="navbar navbar-expand-lg navbar-dark fixed-top shadow-sm custom-navbar">
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm fixed-top">
     <div class="container">
-        <a class="navbar-brand fw-bold" href="#home">
+        <a class="navbar-brand fw-bold" href="#hero">
             Myo Tun Naung
         </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#mainNavbar">
+            data-bs-target="#mainNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="mainNavbar">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-
+                <li class="nav-item"><a class="nav-link" href="#profile">Profile</a></li>
+                <li class="nav-item"><a class="nav-link" href="#experience">Experience</a></li>
+                <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
+                <li class="nav-item"><a class="nav-link" href="#skills">Skills</a></li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#profile">Profile</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#experience">Experience</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#projects">Projects</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#skills">Skills</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link btn btn-primary text-white px-3 ms-lg-3"
-                       href="#contact">
+                    <a class="btn btn-primary btn-sm ms-lg-3" href="#contact">
                         Contact
                     </a>
                 </li>
-
             </ul>
         </div>
     </div>
